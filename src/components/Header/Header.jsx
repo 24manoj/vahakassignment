692e0ea33f6bfabc37bf8ba3680046c4349b494a
manoj <manoj@isoaccess.com>
2021-05-27 10:20:01 +0530

    Automatic Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.scss';
-import src from '../../Assets/vahak-logo.png';
+import src from '../../Assets/Startup.jpeg';
 export default function Header() {
   return (
     <div className='heade-container'>
-      <img className='header-log' src={src}></img>
+      <img className='header-log' src={src} />
+      <p>Vahak</p>
     </div>
   );
 }

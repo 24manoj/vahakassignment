41f5099bc7b966c8229edcbecc304d5c28f522ab
manoj <manoj@isoaccess.com>
2021-05-27 11:51:15 +0530

    May 27, 2021 11:51 AM

--- src/components/SubHeader/SubHeader.jsx
+++ src/components/SubHeader/SubHeader.jsx
@@ -3,7 +3,7 @@ import './subheader.scss';
 export default function SubHeader() {
   return (
     <div className='subheader-appbar'>
-      <p className='subheader-text'>Place your bid</p>
+      <span className='subheader-text'>Place your bid</span>
     </div>
   );
 }

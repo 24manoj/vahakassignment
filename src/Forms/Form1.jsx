f5573d6370f6cc031dee14f7060c4185892d7797
manoj <manoj@isoaccess.com>
2021-05-27 12:00:00 +0530

    May 27, 2021 12:00 PM

--- src/Forms/Form1.jsx
+++ src/Forms/Form1.jsx
@@ -5,12 +5,14 @@ export default function Form1() {
   return (
     <>
       <div>
-        <Field name='source' id='source' type='text' />
-        <ErrorMessage name='source' />
-      </div>
-      <div>
-        <Field name='destination' id='destination' type='text' />
-        <ErrorMessage name='destination' />
+        <div>
+          <Field name='source' id='source' type='text' />
+          <ErrorMessage name='source' />
+        </div>
+        <div>
+          <Field name='destination' id='destination' type='text' />
+          <ErrorMessage name='destination' />
+        </div>
       </div>
       <div>
         <Field as='select' name='cartype'>

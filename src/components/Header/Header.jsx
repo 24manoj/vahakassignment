3ef1a75deb5d99de9c8e45cedfc93e064ac995cf
manoj <manoj@isoaccess.com>
2021-05-27 11:36:34 +0530

    May 27, 2021 11:36 AM

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,10 @@ import src from '../../Assets/Startup.jpeg';
 export default function Header() {
   return (
     <div className='heade-container'>
-      <div className='header-div'>
-        <p>
-          <img className='header-log' src={src} />
-          Vahak
-        </p>
-      </div>
+      <p className='header-div'>
+        <img className='header-log' src={src} />
+        Vahak
+      </p>
     </div>
   );
 }
